ac53d692e793fa83ac62c9b4979617110b72750a
iqbalwaldan <iqbalwaldan0510@gmail.com>
2020-08-20 20:44:39 +0700

    Membuat folder 03-alur-program-python

---
+++ 03-alur-program-python/main.py
@@ -0,0 +1,20 @@
+import time
+start_time = time.time()
+
+print("hellow")
+print("World")
+print("Hello World")
+
+print("Hello cantik")
+# ini adalah komen
+a = 10 # ini dalah komen juga
+"""ada apa dengan 
+ucup dan otong si ganteng
+dalam komen multiline"""
+print(a)
+print(time.time() - start_time, "detik")
+# kita bisa mengcompile python ke
+# yang namanya bytecode
+# cara mengcompile, buka terminal dan tuliskan
+# python -m py_compile Main.py
+
