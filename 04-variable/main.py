d49601b29a116402302bb3a074b5d7bdc02bbcc8
iqbalwaldan <iqbalwaldan0510@gmail.com>
2020-08-20 21:04:37 +0700

    Membuat folder 04-variable

---
+++ 04-variable/main.py
@@ -0,0 +1,26 @@
+# Variable adalah tempat menyimpan data
+
+# menaru / assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+print("Nilai a = ", a)
+print("Nilai a = ", x)
+print("Nilai a = ", panjang)
+
+# Penamaan
+nilai_y = 15 # dengan menggunakan underscore
+# 10juta = 10000000 ini tidak boleh
+juta10 = 10000000 # ini boleh
+nilaiZ = 17.5
+
+# pemanggilan kedua 
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+# assignment indirect
+b = a
+print("Nilai b = ", b)
+
