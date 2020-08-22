892fc3fe375c98c744c7afb26011cc2a537454c0
iqbalwaldan <iqbalwaldan0510@gmail.com>
2020-08-22 20:10:27 +0700

    membuat program konfersi suhu celcius ke suhu lainnya

---
+++ 09-latihan-program-perhitungan-sederhana/main.py
@@ -0,0 +1,22 @@
+# latihan konfersi satuan temperature
+
+# progtam konversi celcius ke satuan lain
+
+print("\nPROGRAM KONVERSI TEMPERATUR\n")
+
+celcius = float(input("Masukkan suhu dalam celcius :"))
+print("suhu dalam celcius adalah",celcius,"Celcius")
+
+# reamur
+reamur = (4/5) * celcius
+print("suhu dalam reamur adalah",reamur,"reamur")
+
+# fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("suhu dalam fahrenheit adalah",fahrenheit,"fahrenheit")
+
+# kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah",kelvin,"kelvin")
+
+
